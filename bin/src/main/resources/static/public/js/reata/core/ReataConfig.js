7ea33ba2465442ff79eb7b602b0c1e9d84b5a2c3
codedao88 <codedao88@gmail.com>
2019-09-27 14:36:30 +0700

    Cập nhật proc signin

--- bin/src/main/resources/static/public/js/reata/core/ReataConfig.js
+++
@@ -1,5 +0,0 @@
-var ReataConfig = function(){
-	this.AuthenUrl = '/reata/api/v1/public/authen/signin';
-	this.DataUrl = '/reata/api/v1.0/public/data/cmd';
-	this.HomeUrl = '/reata/app/common/dashboard';
-}
